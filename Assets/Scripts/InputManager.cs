448322bca20574a3059671f56784d945716d4f0d
lesterlai202333 <141781415+lesterlai202333@users.noreply.github.com>
2024-06-09 15:25:00 +1200

    Sprint 2
    Improving on the movement system

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.Player_OnfootActions onFoot;
+    private PlayerMotor motor;
+    private PlayerLook look;
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.Player_Onfoot;
+        motor = GetComponent <PlayerMotor> ();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.LookAround.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
---
+++ Assets/Scripts/Player Input/PlayerMotor.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 5f;
+    private bool isGrounded;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+        }
+    }
+}
